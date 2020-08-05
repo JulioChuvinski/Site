6bdeda2cb99a061654be07c6c8c9baf46f36dd57
JulioChuvinski <jcchuvinski@hotmail.com>
2020-08-05 20:12:02 -0300

    Insertion Lines
    Insertion for lines of training

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,3 +2,6 @@ nome = input('Qual é o seu nome?')
 idade = input('Qual é a sua idade?')
 peso = input('Qual é o seu peso?')
 Print(nome, idade, peso)
+
+nome = input('Qual é o seu nome?')
+Print('Olá', nome ,'Seja muito bem vindo ficamos muito feliz em ter você aqui')
